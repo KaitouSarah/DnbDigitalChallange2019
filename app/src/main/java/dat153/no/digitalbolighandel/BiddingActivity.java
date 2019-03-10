d84c322a4fbddf1be4dfc94ec8fe1df14bd85945
Liv Sarah Ansnes <182021@stud.hvl.no>
2019-03-10 15:10:22 +0100

    To busy for good commenting. Finished prototype

--- app/src/main/java/dat153/no/digitalbolighandel/BiddingActivity.java
+++ app/src/main/java/dat153/no/digitalbolighandel/BiddingActivity.java
@@ -3,6 +3,7 @@ package dat153.no.digitalbolighandel;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
 import android.view.View;
 
 public class BiddingActivity extends AppCompatActivity {
@@ -12,11 +13,21 @@ public class BiddingActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bidding);
 
+        CardView cardView=findViewById(R.id.biddingSession0);
+        System.out.println(cardView);
+        cardView.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent=new Intent(getApplicationContext(),BiddingRoundActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     public void biddingBackButton(View view) {
         Intent goToPreviousActivity = new Intent(this, MainActivity.class);
         startActivity(goToPreviousActivity);
     }
-
 }
--- app/src/main/java/dat153/no/digitalbolighandel/BiddingRoundActivity.java
+++ app/src/main/java/dat153/no/digitalbolighandel/BiddingRoundActivity.java
@@ -1,7 +1,9 @@
 package dat153.no.digitalbolighandel;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class BiddingRoundActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class BiddingRoundActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bidding_round);
+    }
+
+    public void biddingRoundBackButton(View view) {
+        Intent goToPreviousActivity = new Intent(this, MainActivity.class);
+        startActivity(goToPreviousActivity);
     }
 }
