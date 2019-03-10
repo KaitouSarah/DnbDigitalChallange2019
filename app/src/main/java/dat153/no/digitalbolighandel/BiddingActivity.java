c5f5c2812aefd1aea7e04c87c1c84f546cd7afbb
Liv Sarah Ansnes <182021@stud.hvl.no>
2019-03-10 11:14:50 +0100

    temp

--- app/src/main/java/dat153/no/digitalbolighandel/BiddingActivity.java
+++ app/src/main/java/dat153/no/digitalbolighandel/BiddingActivity.java
@@ -3,6 +3,7 @@ package dat153.no.digitalbolighandel;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
 import android.view.View;
 
 public class BiddingActivity extends AppCompatActivity {
@@ -11,10 +12,12 @@ public class BiddingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bidding);
+
     }
 
     public void biddingBackButton(View view) {
         Intent goToPreviousActivity = new Intent(this, MainActivity.class);
         startActivity(goToPreviousActivity);
     }
+
 }
---
+++ app/src/main/java/dat153/no/digitalbolighandel/BiddingRoundActivity.java
@@ -0,0 +1,13 @@
+package dat153.no.digitalbolighandel;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BiddingRoundActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bidding_round);
+    }
+}
