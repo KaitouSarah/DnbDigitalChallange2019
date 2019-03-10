684a512c9a928760c2c1300dfe7fa08db5fe784e
Liv Sarah Ansnes <182021@stud.hvl.no>
2019-03-10 11:50:38 +0100

    Don't know what I fixed, but it works!

--- app/src/main/java/dat153/no/digitalbolighandel/BiddingActivity.java
+++ app/src/main/java/dat153/no/digitalbolighandel/BiddingActivity.java
@@ -3,7 +3,6 @@ package dat153.no.digitalbolighandel;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.CardView;
 import android.view.View;
 
 public class BiddingActivity extends AppCompatActivity {
--- app/src/main/java/dat153/no/digitalbolighandel/MainActivity.java
+++ app/src/main/java/dat153/no/digitalbolighandel/MainActivity.java
@@ -3,7 +3,6 @@ package dat153.no.digitalbolighandel;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Display;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
